4da1768b16914cdb047600e0dce1e8ead8413a0f
HassanRamos <ramoshassan42@gmail.com>
2020-02-17 15:10:15 +0300

    added file

--- app.js
+++ app.js
@@ -1,41 +1,169 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+'use strict';
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const http = require('http');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const host = 'api.openweathermap.org/';
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const wwoApiKey = '303bfc17dc8102d581d1c1dbb10eb857';
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-module.exports = app;
+exports.weatherWebhook = (req, res) => {
+
+
+  // Get the city and date from the request
+
+
+  let city = req.body.result.parameters['geo-city']; // city is a required param
+
+
+  // Get the date for the weather forecast (if present)
+
+
+  let date = '';
+
+
+  if (req.body.result.parameters['date']) {
+
+
+    date = req.body.result.parameters['date'];
+
+
+    console.log('Date: ' + date);
+
+
+  }
+
+
+  // Call the weather API
+
+
+  callWeatherApi(city, date).then((output) => {
+
+
+    // Return the results of the weather API to API.AI
+
+
+    res.setHeader('Content-Type', 'application/json');
+
+
+    res.send(JSON.stringify({ 'speech': output, 'displayText': output }));
+
+
+  }).catch((error) => {
+
+
+    // If there is an error let the user know
+
+
+    res.setHeader('Content-Type', 'application/json');
+
+
+    res.send(JSON.stringify({ 'speech': error, 'displayText': error }));
+
+
+  });
+
+
+};
+
+
+function callWeatherApi (city, date) {
+
+
+  return new Promise((resolve, reject) => {
+
+
+    // Create the path for the HTTP request to get the weather
+
+
+    let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
+
+
+      '&q=' + encodeURIComponent(city) + '&key=' + wwoApiKey + '&date=' + date;
+
+
+    console.log('API Request: ' + host + path);
+
+
+    // Make the HTTP request to get the weather
+
+
+    http.get({host: host, path: path}, (res) => {
+
+
+      let body = ''; // var to store the response chunks
+
+
+      res.on('data', (d) => { body += d; }); // store each response chunk
+
+
+      res.on('end', () => {
+
+
+        // After all the data has been received parse the JSON for desired data
+
+
+        let response = JSON.parse(body);
+
+
+        let forecast = response['data']['weather'][0];
+
+
+        let location = response['data']['request'][0];
+
+
+        let conditions = response['data']['current_condition'][0];
+
+
+        let currentConditions = conditions['weatherDesc'][0]['value'];
+
+
+        // Create response
+
+
+        let output = `Current conditions in the ${location['type']} 
+
+
+        ${location['query']} are ${currentConditions} with a projected high of
+
+
+        ${forecast['maxtempC']}°C or ${forecast['maxtempF']}°F and a low of 
+
+
+        ${forecast['mintempC']}°C or ${forecast['mintempF']}°F on 
+
+
+        ${forecast['date']}.`;
+
+
+        // Resolve the promise with the output text
+
+
+        console.log(output);
+
+
+        resolve(output);
+
+
+      });
+
+
+      res.on('error', (error) => {
+
+
+        reject(error);
+
+
+      });
+
+
+    });
+
+
+  });
+
+}
